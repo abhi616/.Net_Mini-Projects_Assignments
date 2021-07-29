da7c0e5d350cbe70d55afd35db34c4a099a9c9ee
Abhi Yadav <ay891241@gmail.com>
2021-07-29 17:47:10 +0530

    Assignment 03

---
+++ Assignment_3/Student_Management_System/Student_Management_System/frm_MDI_Student_Management_System.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_MDI_Student_Management_System : Form
+    {
+        private int childFormNumber = 0;
+
+        public frm_MDI_Student_Management_System()
+        {
+            InitializeComponent();
+        }
+
+        private void MDI_Student_Management_System_Load(object sender, EventArgs e)
+        {
+            lbl_Head.Text = "Welcome" + " , " +Global_Vars.Uname;
+            lbl_Head.Visible = true;
+        }
+
+        private void addNewStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Student Obj = new frm_Add_New_Student();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void searchStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Search_Student Obj = new frm_Search_Student();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+    }
+
+}
+      
---
+++ Assignment_3/Student_Management_System/Student_Management_System/frm_Search_Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_Search_Student : Form
+    {
+        public frm_Search_Student()
+        {
+            InitializeComponent();
+        }
+    }
+}
