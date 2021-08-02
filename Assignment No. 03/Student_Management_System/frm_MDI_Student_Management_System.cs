8a8a0558c4a8361acc20b1b1b56f7f42dda990cc
Abhi Yadav <ay891241@gmail.com>
2021-08-02 15:33:06 +0530

    Assignment 03

---
+++ Assignment No. 03/Student_Management_System/frm_MDI_Student_Management_System.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_MDI_Student_Management_System : Form
+    {
+        private int childFormNumber = 0;
+
+        public frm_MDI_Student_Management_System()
+        {
+            InitializeComponent();
+        }
+
+        private void MDI_Student_Management_System_Load(object sender, EventArgs e)
+        {
+            lbl_Head.Text = "Welcome" + " , " +Global_Vars.Uname;
+            lbl_Head.Visible = true;
+        }
+
+        private void addNewStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Student Obj = new frm_Add_New_Student();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void searchStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Search_Student Obj = new frm_Search_Student();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void viewAllStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_View_All_Student Obj = new frm_View_All_Student();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void updateStudentDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Update_Student_Detail Obj = new frm_Update_Student_Detail();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            frm_Login Obj = new frm_Login();
+
+            Obj.Show();
+            this.Hide();
+
+        }
+
+        private void viewStudentByCourseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_View_Student_By_Course Obj = new frm_View_Student_By_Course();
+
+            Obj.Show();
+            this.Hide();
+
+        }
+
+    }
+
+}
+      
---
+++ Assignment No. 03/Student_Management_System/frm_View_All_Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_View_All_Student : Form
+    {
+        public frm_View_All_Student()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            frm_MDI_Student_Management_System Obj = new frm_MDI_Student_Management_System();
+
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void frm_View_All_Student_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'student_Management_System_DBDataSet2.Student_Details' table. You can move, or remove it, as needed.
+            this.student_DetailsTableAdapter2.Fill(this.student_Management_System_DBDataSet2.Student_Details);
+            // TODO: This line of code loads data into the 'student_Management_System_DBDataSet1.Student_Details' table. You can move, or remove it, as needed.
+            this.student_DetailsTableAdapter1.Fill(this.student_Management_System_DBDataSet1.Student_Details);
+            // TODO: This line of code loads data into the 'student_Management_System_DBDataSet.Student_Details' table. You can move, or remove it, as needed.
+            this.student_DetailsTableAdapter.Fill(this.student_Management_System_DBDataSet.Student_Details);
+
+        }
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            frm_Login Obj = new frm_Login();
+
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
