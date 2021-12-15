64bf767f707a4fb83ccccf30e5ea50baa9edc4fe
Abhi Yadav <ay891241@gmail.com>
2021-12-15 21:21:37 +0530

    Add files via upload

---
+++ Console Application/LB Assignments/Assignment_01/03/03/Program.cs
@@ -0,0 +1,32 @@
+﻿
+/// 03. Program to print 5 to 1 numbers on screen.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03
+{
+    class Program
+    {
+        static void Display()
+        {
+            int i = 5;
+
+            for (; i > 0; i--)
+            {
+                Console.WriteLine("\n\t\t"+i);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t-------- Output --------");
+            Display();
+            
+            Console.ReadKey();
+        }
+    }
+}
